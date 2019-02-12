c8db55ab27804deed9c784c11019c78703e80314
Eric Heayn <34361401+ericheayn@users.noreply.github.com>
2019-02-11 23:43:14 -0500

    Finished function and tests

---
+++ index.test.js
@@ -0,0 +1,85 @@
+const ackermann = require('./index')
+
+test('calculates (0,0) to equal 1', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (0,1) to equal 2', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (0,2) to equal 3', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (0,3) to equal 4', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (0,4) to equal 5', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (1,0) to equal 2', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (1,1) to equal 3', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (1,2) to equal 4', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (1,3) to equal 5', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (1,4) to equal 6', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (2,0) to equal 3', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (2,1) to equal 5', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (2,2) to equal 7', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (2,3) to equal 9', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (2,4) to equal 11', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (3,0) to equal 5', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (3,1) to equal 13', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (3,2) to equal 29', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (3,3) to equal 61', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (3,4) to equal 125', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
+
+test('calculates (4,0) to equal 13', () => {
+  expect(ackermann(0,0)).toBe(1);
+});
